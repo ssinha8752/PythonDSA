bdfae166869fd13dff05b8c7d6dafd53ea957d52
ssinha8752 <shubhamsinha8752@yahoo.com>
2024-08-06 03:06:52 +0530

    [Shubham][6-Aug] Python DSA

--- DSA/BigO.py
+++ DSA/BigO.py
@@ -1,7 +1,18 @@
-#O(n)
+#O(n) - propotional time, average case
+#def print_items(n):
+#    for i in range(n):
+#        print(i)
+#print_items(10)
 
-def print_items(n):
-    for i in range(n):
-        print(i)
+#O(n^2) - exponential time, worst case
+#def print_items(n):
+#    for i in range(n):
+#        for j in range(n):
+#            print(i,j)
+#print_items(10)
 
-print_items(10)
+#O(1)- constant time, best case
+#def print_items(n):
+#    for i in range(n):
+#        print(i+i)
+#print_items(10)
