16aa9bf5ffaaf22ea9e4bc3a628440ad75efb7dc
ssinha8752 <shubhamsinha8752@yahoo.com>
2024-08-09 02:53:47 +0530

    [Shubham][7-Aug] Python DSA - Linked List

---
+++ DSA/LL.py
@@ -0,0 +1,15 @@
+class Node:
+    def __init__(self,value):
+        self.value=value
+        self.next=None
+
+class LinkedList:
+    def __init__(self,value):
+        new_node=Node(value)
+        self.head=new_node
+        self.tail=new_node
+        self.length=1
+
+ll=LinkedList(4)
+
+print(ll.tail.value)
