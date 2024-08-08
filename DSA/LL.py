aeb56c546dfce0278019bd57ca765492781155d8
ssinha8752 <shubhamsinha8752@yahoo.com>
2024-08-09 03:06:57 +0530

    [Shubham][9-Aug] Python DSA - Linked List

--- DSA/LL.py
+++ DSA/LL.py
@@ -10,6 +10,25 @@ class LinkedList:
         self.tail=new_node
         self.length=1
 
+    def print_list(self):
+        temp=self.head
+        while temp is not None:
+            print(temp.value)
+            temp=temp.next
+
+    def append(self,value):
+        new_node=Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next=new_node
+            self.tail=new_node
+        self.length+=1
+
+
 ll=LinkedList(4)
+ll.append(5)
+ll.print_list()
 
 print(ll.tail.value)
